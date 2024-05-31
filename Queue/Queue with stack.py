aee6c9f40c6c1117a69eee4ef2e577d2b3184329
kumarideepali0402 <154409860+kumarideepali0402@users.noreply.github.com>
2024-05-31 23:48:06 +0530

    done

--- Queue/Queue with stack.py
+++ Queue/Queue with stack.py
@@ -1,25 +1,39 @@
-# class Node:
-#     def __init__(self,data,next=None)
-#      self.data=data
-#      self.next=next
-# class Stack:
-#     def __init__(self):
-#      self.__head=None
-#      self.__tail=None
-#      self.__size=0
-#     def size(self):
-#        return self.__size
+class Node:
+    def __init__(self,data,next=None)
+     self.data=data
+     self.next=next
+class Stack:
+    def __init__(self):
+     self.__head=None
+     self.__tail=None
+     self.__size=0
+    def size(self):
+       return self.__size
     
-#     def isEmpty(self):
-#        return self.size()==0
+    def isEmpty(self):
+       return self.size()==0
     
-#     def push(self):
-#         newNode=Node()
-#         if self.isEmpty():
-#           self.__head=newNode
-#           self.__tail=newNode
-#         else:
-           
-#     elf.__tail.next=sel
+    def push(self,data):
+        newNode=Node()
+        if self.isEmpty():
+          self.__head=newNode
+          self.__tail=newNode
+        else:
+          self.__tail.next=newNode
+          self.__tail=newNode
+
+    def pop(self):
+       if self.isEmpty():
+          raise Exception("Stack is empty")
+       else:
+          trav=self.__head
+          while trav.next.next:
+             trav=trav.next
+          trav.next=None
+          self.__tail=trav
+
+l=Stack()
+l.push()
+       
 
 
