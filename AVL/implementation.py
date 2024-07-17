73efe79db7c6ccfd85e24898a6bebad4c07d405c
kumarideepali0402 <154409860+kumarideepali0402@users.noreply.github.com>
2024-07-17 23:09:57 +0530

    avl

---
+++ AVL/implementation.py
@@ -0,0 +1,161 @@
+class Node:
+    def __init__(self,data,right=None,left=None):
+        self.data=data
+        self.right=right
+        self.left=left
+        self.height=0
+        self.balanceFactor=0
+
+class AVL:
+    def __init__(self):
+        self.__nodesCount=0
+        self.__root=None
+
+    def find(self,value):
+        return self.__contains(self.__root,value)
+        
+    def __contains(self,node,data):
+        if node is None:
+            return False
+        if (self.__node.value==data):
+            return True
+        elif(node.value>data):
+            return self.__contains(self.__root.right,data)
+        elif(self.__root.value<data):
+            return self.__contains(self.__root.left,data)
+        return False
+    
+    def insert(self,data):
+        if (data==None):
+            return False
+        if(self.find(data)):
+            return False
+        else:
+            self.root=self.insert2(self.root,data)
+            self.__nodesCount+=1
+            return True
+    
+    def insert2(self,node,data):
+        if node is None:
+            return Node(data)
+        if(node.data>data):
+            node.left= self.insert2(node.left,data)
+        if(node.data<data):
+            node.right= self.insert2(node.right,data)
+        
+        self.__update(node)
+
+        return self.__balance(node)
+    def __update(self,node):
+        leftHeight,rightHeight=-1,-1
+        if node.left is not None:
+            leftHeight=node.left.height
+        if node.right is not None:
+            rightHeight=node.right.height
+        
+        node.height=1+max(leftHeight,rightHeight)
+        node.balanceFactor=rightHeight-leftHeight
+
+    
+    def __balance(self,node):
+        if node.balancefactor == +2:
+            if(node.right.balanceFactor > 1):
+                return self.__rightRightCase(node)
+            else:
+                return self.__rightLeftCase(node)
+        elif(node.balanceFactor == -2):
+            if(node.right.balanceFactor>0):
+                return self.__leftleftCase(node)
+            else:
+                return self.__leftRightCase(node)
+            
+    def __rightRightCase(self,node):
+        return self.__rotateRight(self,node)
+    
+    def __rightLeftCase(self,node):
+        node.right=self.__rotateRight(node.right)
+        return self.__rightRightCase(node)
+    
+    def __leftLeftCase(self,node):
+        return self.__leftLeftCase(node)
+    
+    def __leftRightCase(self,node):
+        node.left=self.__rotateLeft(node.left)
+        return self.__leftLeftCase(node)
+    
+    def __rotateLeft(self,node):
+        b=node.left
+        node.left=b.right
+        b.right=node
+
+        self.__update(node)
+        self.__update(b)
+        return b
+    
+    def __rotateRight(self,node):
+        b=node.right
+        node.right=b.left
+        b.left=node
+        self.__update(node)
+        self.__update(b)
+
+        return b
+    
+    def remove(self,value):
+        if value==None:
+            return False
+        if(self.find(value)==False): 
+            return False
+        else:
+            self.root=self.__remove(self.root,value)
+            self.__nodesCount-=1
+            return True
+        
+    def __remove(self,node,value):
+        if(node.data==value):
+            if node.right is None and node.left is None:
+                del(node)
+                return None
+            elif node.left is None:
+                temp=node.right
+                del(node)
+                return temp
+            elif node.right is None:
+                temp=node.left
+                del(node)
+                return temp
+            else:
+                successor=node.right
+                while successor is not None:
+                    successor=successor.left
+                node.data=successor.data
+                node.right=self.__remove(node.right,successor.data)
+                return node
+        elif(node.data>value):
+            node.left= self.__remove(node.left,value)
+        else:
+            node.right= self.__remove(node.right,value)
+
+            
+        self.__update(node)
+
+        return self.__balance(node)
+
+
+
+
+
+        
+    
+
+
+            
+
+
+    
+        
+
+
+        
+
+
--- BST/implementatuion.py
+++ BST/implementatuion.py
@@ -1,70 +1,70 @@
-class Node:
-    def __init__(self,data,left=None,right=None):
-        self.data=data
-        self.left=left
-        self.right=right
+# class Node:
+#     def __init__(self,data,left=None,right=None):
+#         self.data=data
+#         self.left=left
+#         self.right=right
         
-class BST:
-    def __init__(self):
-        self.root=None
-        # self.size=0
-    def insert2(self,root,data):
-        #insert value in myBST and return the update tree
-        if(root==None):
-            return Node(data)
-        if(root.data==data):
-            return root
-        if(data<root.data):
-            root.left=self.insert(root.left,data)
-            return root
-        else :
-            root.right=self.insert(root.right,data)
-            return root
+# class BST:
+#     def __init__(self):
+#         self.root=None
+#         # self.size=0
+#     def insert2(self,root,data):
+#         #insert value in myBST and return the update tree
+#         if(root==None):
+#             return Node(data)
+#         if(root.data==data):
+#             return root
+#         if(data<root.data):
+#             root.left=self.insert(root.left,data)
+#             return root
+#         else :
+#             root.right=self.insert(root.right,data)
+#             return root
         
-    def insert(self,data):
-        self.root=self.insert2(self.root,data)
-        return self.root.data
+#     def insert(self,data):
+#         self.root=self.insert2(self.root,data)
+#         return self.root.data
     
     
-    def inorder(self,root):
-        if(root==None): 
-            return
-        self.inorder(self.left)
-        print(root.data)
-        self.inorder(self.right)
+#     def inorder(self,root):
+#         if(root==None): 
+#             return
+#         self.inorder(self.left)
+#         print(root.data)
+#         self.inorder(self.right)
 
-    def printTree(self):
-        self.inorder(self.root)
+#     def printTree(self):
+#         self.inorder(self.root)
     
-    def remove(self,data):
-        self.root=remove2(self.root,data)
-    def remove2(self,root,data):
-        if(root==None):
-            return root
-        if(root.data>data):
-            root.left=self.remove2(root.left,data)
-            return root
-        if(root.data<data):
-            root.right=self.remove2(root.right,data)
-            return root
-        if(root.data==data):
-            if(root.left==None and root.right==None):
-                del root
-                return None
-            if(root.right==None ):
-                temp=root.left
-                del root
-                return temp
-            if(root.left==None):
-                temp=root.right
-                del root
-                return temp
-            else:
-                succ=root.left
-                while succ.right is not None:
-                    succ=succ.right
-                root.data=succ.data
-                remove2(succ.data)
+#     def remove(self,data):
+#         self.root=remove2(self.root,data)
+#     def remove2(self,root,data):
+#         if(root==None):
+#             return root
+#         if(root.data>data):
+#             root.left=self.remove2(root.left,data)
+#             return root
+#         if(root.data<data):
+#             root.right=self.remove2(root.right,data)
+#             return root
+#         if(root.data==data):
+#             if(root.left==None and root.right==None):
+#                 del root
+#                 return None
+#             if(root.right==None ):
+#                 temp=root.left
+#                 del root
+#                 return temp
+#             if(root.left==None):
+#                 temp=root.right
+#                 del root
+#                 return temp
+#             else:
+#                 succ=root.left
+#                 while succ.right is not None:
+#                     succ=succ.right
+#                 root.data=succ.data
+#                 remove2(succ.data)
             
 
 
@@ -73,7 +73,74 @@ class BST:
 
 
 
-newnode=Node(13)
-print(newnode.data,newnode.left,newnode.right)
-newnode.insert(12)
-newNode.print()
+# newnode=Node(13)
+# print(newnode.data,newnode.left,newnode.right)
+# newnode.insert(12)
+# newNode.print()
+
+
+
+class Node:
+    def __init__(self, data, left=None, right=None):
+        self.data = data
+        self.left = left
+        self.right = right
+        
+class BST:
+    def __init__(self):
+        self.root = None
+    
+    def insert2(self, root, data):
+        if root is None:
+            return Node(data)
+        if root.data == data:
+            return root
+        if data < root.data:
+            root.left = self.insert2(root.left, data)
+        else:
+            root.right = self.insert2(root.right, data)
+        return root
+        
+    def insert(self, data):
+        self.root = self.insert2(self.root, data)
+        return self.root.data
+    
+    def inorder(self, root):
+        if root is None:
+            return
+        self.inorder(root.left)
+        print(root.data)
+        self.inorder(root.right)
+
+    def printTree(self):
+        self.inorder(self.root)
+    
+    def remove(self, data):
+        self.root = self.remove2(self.root, data)
+    
+    def remove2(self, root, data):
+        if root is None:
+            return root
+        if data < root.data:
+            root.left = self.remove2(root.left, data)
+        elif data > root.data:
+            root.right = self.remove2(root.right, data)
+        else:
+            if root.left is None and root.right is None:
+                return None
+            if root.right is None:
+                return root.left
+            if root.left is None:
+                return root.right
+
+            # Find the inorder successor (smallest in the right subtree)
+            temp = self.findMin(root.right)
+            root.data = temp.data
+            root.right = self.remove2(root.right, temp.data)
+        return root
+
+    def findMin(self, root):
+        current = root
+        while current.left is not None:
+            current = current.left
+        return current
